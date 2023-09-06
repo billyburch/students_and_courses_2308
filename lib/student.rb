57638e8871f4b288403f5a36a659a0c8da00a10c
Billy Burch <billyburch40@gmail.com>
2023-09-06 10:18:13 -0600

    add class code for 4 of 5 tests iteration 1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
 class Student
+attr_reader :name, :age, :scores
   
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @age = attributes.fetch(:age)
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade(score_array)
+    score_array = @scores
+    sum = 0
+    score_array.each do |score|
+      sum += score
+    end
+    grade = sum.to_f / score_array.length
+    return grade
+  end
 end
